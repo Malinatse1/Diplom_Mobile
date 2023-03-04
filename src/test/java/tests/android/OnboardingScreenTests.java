ada65fb4558685899ecc9c960c68a7d506aef644
Natalya Bochkova <bochkovanatalya7@gmail.com>
2023-03-04 19:06:44 +0300

    added tests

--- src/test/java/tests/android/OnboardingScreenTests.java
+++ src/test/java/tests/android/OnboardingScreenTests.java
@@ -1,12 +1,9 @@
-package tests.android.local;
+package tests.android;
 
 import org.junit.jupiter.api.Test;
 
-import static com.codeborne.selenide.CollectionCondition.sizeGreaterThan;
 import static com.codeborne.selenide.Condition.text;
-import static com.codeborne.selenide.Condition.visible;
 import static com.codeborne.selenide.Selenide.*;
-import static io.appium.java_client.AppiumBy.accessibilityId;
 import static io.appium.java_client.AppiumBy.id;
 import static io.qameta.allure.Allure.step;
 import static org.junit.jupiter.api.Assertions.assertEquals;
